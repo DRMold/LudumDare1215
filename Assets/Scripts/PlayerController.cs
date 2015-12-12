e41099480273583334e72ddafe053f7fcee6ce9e
Ritchie Hofmann <rhofma2@lsu.edu>
2015-12-11 22:51:38 -0600

    Created PlayerController.cs

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+
+	private Rigidbody myBody;
+
+	void Start () {
+		myBody = GetComponent<Rigidbody> ();
+	}
+
+	void Update () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+
+		myBody.AddForce (movement * speed);
+	}
+}
