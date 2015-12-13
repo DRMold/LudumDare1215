3f3e8e4bb82ffb5eb7d03c41b381b7862c7c2341
Ritchie Hofmann <rhofma2@lsu.edu>
2015-12-13 13:39:21 -0600

    Preparing to pull Civillian AI

---
+++ Assets/Scripts/PauseUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PauseMenu_UI : MonoBehaviour {
+	public Canvas pauseMenuCanvas;
+	
+	// Make sure the pause system is properly displaying upon start of level
+	void Start () {
+		pauseMenuCanvas.enabled = false;
+	}
+	
+	// Models the behavior for Pause Button
+	public void PausePressed() {
+		pauseMenuCanvas.enabled = true;
+		
+		Time.timeScale = 0; // This pauses the game
+	}
+	
+	// Models the behavior for Resume Button
+	public void ResumePressed() {
+		pauseMenuCanvas.enabled = false;
+		
+		Time.timeScale = 1; // This resumes the game
+	}
+	
+	// Models the behavior for Quit Button
+	public void QuitPressed() {
+		Time.timeScale = 1; 
+	}
+}
