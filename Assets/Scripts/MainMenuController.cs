aa54a6511be05ec84ee71c195b9c35f1185b91dd
Kamekai <creative.rebirth@gmail.com>
2015-12-13 17:08:09 -0600

    x-slider

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour {
+	public Camera cam;
+	// Use this for initialization
+	void Start () {
+		cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)){ // if left button pressed...
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit)){
+				if (hit.transform.gameObject.name == "Start") {
+					// DO STUFF HERE
+				}
+			}
+		}
+	}
+}
