3ce105191f3cd109c618bedf4ad9509e39045f12
Kamekai <creative.rebirth@gmail.com>
2015-12-13 23:38:06 -0600

    adding civilian and anim

---
+++ Assets/Scripts/AnimationInputTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationInputTest : MonoBehaviour {
+
+	private Animation anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animation>();
+		anim["Run"].speed = 10.0f; 
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (anim.isPlaying == false) {
+			if (true) {
+				anim.Play("Run");
+			}
+		}
+	}
+}
